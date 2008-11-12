ea58286cc1dd043cbb9463c50a79e9e263eeee87
dan <dan@ef174c80-dc52-11dd-a8eb-ddf07f3c1838>
2008-11-12 00:14:54 +0000

    #3 and #6.

--- mantis_rest/trunk/crap.py
+++ mantis_rest/trunk/crap.py
@@ -15,14 +15,50 @@ c = httplib.HTTPConnection('mantis.localhost')
 headers = dict(Authorization = 'Basic %s' % authdata,
                Accept = 'application/json')
 
-body = '{"project_id":"1","reporter":"http://mantis.localhost/rest/users/2","handler":"http://mantis.localhost/rest/users/4","duplicate":"","priority":"30","severity":"50","reproducibility":"70","status":"10","resolution":"10","projection":"10","category":"","date_submitted":1222828053,"last_updated":1223520462,"eta":"10","os":"","os_build":"","platform":"","version":"","fixed_in_version":"","target_version":"","build":"","view_state":"10","summary":"Swizzle","sponsorship_total":"0","sticky":"0","profile_id":"0","description":"Bitch","steps_to_reproduce":"","additional_information":"","_stats":null}'
-c.request('PUT', uri, body=body, headers=headers)
+bug_body = """{"project_id":"1",
+"reporter":"http:\/\/mantis.localhost\/rest\/users\/2",
+"handler":"http:\/\/mantis.localhost\/rest\/users\/4",
+"duplicate":"",
+"priority":"urgent",
+"severity":"feature",
+"reproducibility":"always",
+"status":"assigned",
+"resolution":"open",
+"projection":"tweak",
+"category":"",
+"date_submitted":1222828053,
+"last_updated":1223520462,
+"eta":"< 1 week",
+"os":"",
+"os_build":"",
+"platform":"",
+"version":"",
+"fixed_in_version":"",
+"target_version":"",
+"build":"",
+"private":false,
+"summary":"Swizzle",
+"sponsorship_total":"0",
+"sticky":"0",
+"profile_id":"0",
+"description":"Bitches hey",
+"steps_to_reproduce":"",
+"additional_information":"Oh noes!  A bug!"}"""
+note_body = """{"bug":"http:\/\/mantis.localhost\/rest\/bugs\/1",
+"reporter":"http:\/\/mantis.localhost\/rest\/2",
+"note":"Whoa, note.",
+"private":false,
+"date_submitted":"2008-10-08 22:47:42",
+"last_modified":"2008-10-08 22:47:42"}"""
+c.request('PUT', uri, body=note_body, headers=headers)
+#c.request('GET', uri, headers=headers)
 r = c.getresponse()
 print "Status code: %d" % r.status
 
 headers = r.getheaders()
 output = r.read()
-for h in headers: print h
+for h in headers: print "%s: %s" % h
+print
 print output.rstrip()
 
 #resource = simplejson.loads(r.read())
--- mantis_rest/trunk/resources/bugnote.class.php
+++ mantis_rest/trunk/resources/bugnote.class.php
@@ -1,27 +1,71 @@
 <?php
 class BugNote extends Resource
 {
-	/*
+	/**
 	 *      A bug note.
 	 */
 	function __construct($url)
 	{
-		/*
+		/**
 		 *      Constructs the note.
 		 *
 		 *      @param $url - The URL with which this resource was requested
 		 */
 		$matches = array();
-		if (preg_match('!/notes/(\d+)/?!', $url, &$matches)) {
-			$this->note_id = $matches[1];
+		$this->note_id = BugNote::get_mantis_id_from_url($url);
+	}
+
+	static function get_url_from_mantis_id($id)
+	{
+		/**
+		 * 	Given a Mantis bugnote ID, returns the URL of its resource.
+		 */
+		return $GLOBALS['cfg_api_url'] . "/notes/$id";
+	}
+
+	static function get_mantis_id_from_url($url)
+	{
+		/**
+		 * 	Given the URL of a bugnote, return that note's Mantis ID.
+		 */
+		$matches = array();
+		if (preg_match('!/(\d+)/?$!', $url, &$matches)) {
+			return (int)$matches[1];
 		} else {
-			http_error(404, "No such bug note: $matches[1]");
+			http_error(404, "No resource at $url");
 		}
+	}
+
+	static function get_db_row_from_resource($rsrc)
+	{
+		/**
+		 * 	Given a resource (as an array), return the database row.
+		 */
+		$row = array();
+		$row['bug_id'] = Bug::get_mantis_id_from_url($rsrc['bug']);
+		$row['reporter_id'] = User::get_mantis_id_from_url($rsrc['reporter']);
+		$row['note'] = $rsrc['note'];
+		$row['private'] = !!($rsrc['view_state'] == VS_PRIVATE);
+		$row['date_submitted'] = $rsrc['date_submitted'];
+		$row['last_modified'] = $rsrc['last_modified'];
+		return $row;
+	}
+
+	static function get_resource_from_db_row($row)
+	{
+		$rsrc = array();
+		$rsrc['bug'] = Bug::get_url_from_mantis_id($row['bug_id']);
+		$rsrc['reporter'] = User::get_url_from_mantis_id($row['reporter_id']);
+		$rsrc['note'] = $row['note'];
+		$rsrc['view_state'] = $row['private'] ? VS_PRIVATE : VS_PUBLIC;
+		$rsrc['date_submitted'] = $row['date_submitted'];
+		$rsrc['last_modified'] = $row['last_modified'];
+		return $rsrc;
 	}
 
 	public function get()
 	{
-		/*
+		/**
 		 *      Returns a representation of the note.
 		 */
 		if (!bugnote_exists($this->note_id)) {
@@ -53,18 +97,35 @@ class BugNote extends Resource
 			$cols[$col_names[$i]] = $row[$i];
 		}
 
-		$this->data['bug'] = Bug::get_url_from_mantis_id($cols['bug_id']);
-		$this->data['reporter'] = User::get_url_from_mantis_id($cols['reporter_id']);
-		$this->data['note'] = $cols['note'];
-		$this->data['private'] = !!($cols['view_state'] == VS_PRIVATE);
-		$this->data['date_submitted'] = $cols['date_submitted'];
-		$this->data['last_modified'] = $cols['last_modified'];
+		$this->data = BugNote::get_resource_from_db_row($cols);
 		return $this->repr();
 	}
 
 	public function put()
 	{
-		method_not_allowed("PUT");
+		if (!bugnote_exists($this->note_id)) {
+			http_error(404, "No such bug note: $this->note_id");
+		}
+		# Check if the current user is allowed to edit the bugnote
+		# (This comes from Mantis's bugnote_update.php)
+		$user_id = auth_get_current_user_id();
+		$reporter_id = bugnote_get_field( $f_bugnote_id, 'reporter_id' );
+		if ( ( $user_id != $reporter_id ) ||
+				( OFF == config_get( 'bugnote_allow_user_edit_delete' ) )) {
+			access_ensure_bugnote_level(
+				config_get( 'update_bugnote_threshold' ), $f_bugnote_id );
+		}
+
+		# Check if the bug is readonly
+		$bug_id = bugnote_get_field( $bugnote_id, 'bug_id' );
+		if (bug_is_readonly($bug_id)) {
+			http_error(403, "Access denied: bug is read-only.");
+		}
+
+		$new_rep = file_get_contents('php://input');
+		$new_data = json_decode($new_rep, true);
+		$bugnote_data = BugNote::get_db_row_from_resource($new_data);
+		bugnote_set_text($this->note_id, $bugnote_data->note);
 	}
 }
 ?>
