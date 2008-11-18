48c35f8619af7996d0cbc074390d11585ad58b63
dan <dan@ef174c80-dc52-11dd-a8eb-ddf07f3c1838>
2008-11-18 04:22:15 +0000

    Implemented PUT method for '''User''' resource.  #14.

--- mantis_rest/trunk/resources/user.class.php
+++ mantis_rest/trunk/resources/user.class.php
@@ -31,6 +31,9 @@ class User extends Resource
 	function __construct($url)
 	{
 		$this->user_id = User::get_mantis_id_from_url($url);
+		if (!user_exists($this->user_id)) {
+			http_error(404, "No such user");
+		}
 
 		$this->mantis_data = array();
 		$this->rsrc_data = array();
@@ -97,9 +100,6 @@ class User extends Resource
 			  FROM $user_table u
 			  WHERE u.id='$this->user_id'";
 		$result = db_query($query);
-		if (db_num_rows($result) == 0) {
-			http_error(404, "No user exists with ID $this->user_id");
-		}
 
 		$row = db_fetch_array($result);
 		$col_names = array('username', 'realname', 'email', 'date_created',
@@ -117,7 +117,58 @@ class User extends Resource
 
 	public function put()
 	{
-		method_not_allowed("PUT");
+		if (!access_has_global_level(config_get('manage_user_threshold'))
+				&& auth_get_current_user_id() != $this->user_id) {
+			http_error(403, "Access denied to edit user $this->user_id's info");
+		}
+
+		$new_rep = file_get_contents('php://input');
+		$this->rsrc_data = json_decode($new_rep, TRUE);
+
+		# Do some validation on the inputs (from Mantis's user_create())
+		$username = db_prepare_string($this->rsrc_data['username']);
+		$realname = db_prepare_string($this->rsrc_data['realname']);
+		$password = db_prepare_string($this->rsrc_data['password']);
+		$email = db_prepare_string($this->rsrc_data['email']);
+		$access_level = db_prepare_int(
+			get_string_to_enum(config_get('access_levels_enum_string'),
+				$this->rsrc_data['access_level']));
+		$protected = db_prepare_bool($this->rsrc_data['protected']);
+		$enabled = db_prepare_bool($this->rsrc_data['enabled']);
+
+		user_ensure_name_valid($username);
+		user_ensure_realname_valid($realname);
+		user_ensure_realname_unique($username, $realname);
+		email_ensure_valid($email);
+
+		# The cookie string is based on email and username, so if either of those changed,
+		# we have to change the cookie string.
+		$user_row = user_get_row($this->user_id);
+		$username_key = array_key_exists('username', $user_row) ? 'username' : 1;
+		$email_key = array_key_exists('email', $user_row) ? 'email' : 3;
+		$cookie_string_key = array_key_exists('cookie_string', $user_row) ?
+			'cookie_string' : 13;
+		if ($user_row[$username_key] != $username || $user_row[$email_key] != $email) {
+			$seed = $email . $username;
+			$cookie_string = auth_generate_unique_cookie_string($seed);
+		} else {
+			$cookie_string = $user_row[$cookie_string_key];
+		}
+
+		$password_hash = auth_process_plain_password($password);
+
+		$user_table = config_get('mantis_user_table');
+		$query = "UPDATE  $user_table
+				SET username = '$username',
+				    realname = '$realname',
+				    email = '$email',
+				    password = '$password_hash',
+				    enabled = $enabled,
+				    protected = $protected,
+				    access_level = $access_level,
+				    cookie_string = '$cookie_string'
+				WHERE id = $this->user_id;";
+		db_query($query);
 	}
 }
 ?>
